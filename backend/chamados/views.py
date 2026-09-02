e77c2edce6360ed7cb7da2057acb12e70e3507a3
Kaue Humenhuk Ruchinski <kauehruchinski@gmail.com>
2026-09-01 21:18:49 -0300

    INC-06 — Indicadores de chamados

--- backend/chamados/views.py
+++ backend/chamados/views.py
@@ -1,4 +1,6 @@
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError
 from .models import Chamado
 from .serializers import ChamadoSerializer
@@ -7,33 +9,16 @@ class ChamadoListCreateView(generics.ListCreateAPIView):
     serializer_class = ChamadoSerializer
 
     def get_queryset(self):
-<<<<<<< HEAD
         # 1. Pega todos os chamados
         queryset = Chamado.objects.all()
         
         # 2. Captura o parâmetro 'status' da URL (?status=ABERTO)
         status_param = self.request.query_params.get('status', None)
-=======
-        # 1. Pega todos os chamados do banco
-        queryset = Chamado.objects.all()
-        
-        # 2. Verifica se o usuário enviou o filtro na URL (ex: ?status=ABERTO)
-        status_param = self.request.query_params.get('status')
->>>>>>> 88ffd76c11169db302ade1d70c6dee90dc0a14d4
 
         if status_param is not None:
             status_param = status_param.upper()
             status_permitidos = ['ABERTO', 'EM_ANDAMENTO', 'CONCLUIDO']
             
-<<<<<<< HEAD
-            # 3. Valida se o status enviado é aceito
-            if status_param not in status_permitidos:
-                raise ValidationError({
-                    "status": f"Status '{status_param}' é inválido. Valores aceitos: {', '.join(status_permitidos)}."
-                })
-            
-            # 4. Aplica o filtro no banco de dados
-=======
             # 3. Tratamento de parâmetro inválido (Critério de Aceite)
             if status_param not in status_permitidos:
                 raise ValidationError({
@@ -41,7 +26,6 @@ class ChamadoListCreateView(generics.ListCreateAPIView):
                 })
             
             # 4. Aplica o filtro (Critério de Aceite)
->>>>>>> 88ffd76c11169db302ade1d70c6dee90dc0a14d4
             queryset = queryset.filter(status=status_param)
 
         return queryset
@@ -49,3 +33,17 @@ class ChamadoListCreateView(generics.ListCreateAPIView):
 class ChamadoDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Chamado.objects.all()
     serializer_class = ChamadoSerializer
+
+class IndicadoresView(APIView):
+    def get(self, request):
+        total = Chamado.objects.count()
+        abertos = Chamado.objects.filter(status='ABERTO').count()
+        em_andamento = Chamado.objects.filter(status='EM_ANDAMENTO').count()
+        concluidos = Chamado.objects.filter(status='CONCLUIDO').count()
+
+        return Response({
+            'total': total,
+            'abertos': abertos,
+            'em_andamento': em_andamento,
+            'concluidos': concluidos
+        })
